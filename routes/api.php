50b5c1746ae4c8906fc7829cd4631cd34c8968ff
mdm106 <mads.mcmahon@googlemail.com>
2020-06-13 11:04:20 +0100

    Api routes set up

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\API\Categories;
+use App\Http\Controllers\API\Words;
+use App\Http\Controllers\API\Games;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +19,20 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group(["prefix" => "/categories"], function() {
+    Route::get("", "API\\Categories@index");
+    Route::get("/{category}", "API\\Categories@show");
+    Route::post("", "API\\Categories@store");
+    Route::get("/{category}/words", "API\\Categories\\Words@show");
+    Route::post("/{category}/words", "API\\Categories\\Words@store");
+});
+
+Route::group(["prefix" => "/games"], function() {
+    Route::get("", "API\\Games@index");
+    Route::get("/{game}", "API\\Games@show");
+    Route::post("", "API\\Games@store");
+    Route::delete("/{game}", "API\\Games@destroy");
+    Route::patch("/{game}/score", "API\\Games@score");
 });
