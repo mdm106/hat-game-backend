957c60d14d857b1cc9cb1d847bb9d336b55c7832
mdm106 <mads.mcmahon@googlemail.com>
2020-06-13 09:33:52 +0100

    One to many relationship set up in model for relationship between categories and words

--- app/Category.php
+++ app/Category.php
@@ -5,6 +5,10 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
-{
-    //
+{   
+    //one to many relationship for category to words
+    public function words()
+    {
+        return $this->hasMany(Word::class);
+    }
 }
--- app/Word.php
+++ app/Word.php
@@ -5,6 +5,10 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class Word extends Model
-{
-    //
+{   
+    //one to many relationship for category to words
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
