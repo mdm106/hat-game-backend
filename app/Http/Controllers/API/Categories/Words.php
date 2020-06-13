ad70ba60891c4c2295f02fe782e0e695b8d1eb3f
mdm106 <mads.mcmahon@googlemail.com>
2020-06-13 13:40:57 +0100

    Store for words added to words controller

--- app/Http/Controllers/API/Categories/Words.php
+++ app/Http/Controllers/API/Categories/Words.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Collection;
 use App\Http\Resources\API\WordResource;
 
 use App\Category;
+use App\Word;
 
 class Words extends Controller
 {
@@ -27,9 +28,13 @@ class Words extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request, Category $category)
+    public function store(Request $request)
     {
-    
+        //only taking non id variables from the request
+        $data = $request->only("word", "category_id");
+
+        //store da
+        return Word::create($data);
     }
 
     /**
