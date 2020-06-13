eedef6e0f05729e61b86963c2f51536b83277d8a
mdm106 <mads.mcmahon@googlemail.com>
2020-06-13 10:08:11 +0100

    Functions added to increase scores and to return the winner

--- app/Game.php
+++ app/Game.php
@@ -5,6 +5,25 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class Game extends Model
-{
-    //
+{   
+    //function to increase scores
+    public function score(int $team) : Game
+    {
+        $prop = "score_${team}";
+        $this->$prop += 1;
+        $this->save();
+        return $this;
+    }
+
+    //function to return winner
+    public function winner() : string
+    {
+        if ($this->score_1 > $this->score_2 && $this->complete) {
+            return "1";
+        } else if ($this->score_2 > $this->score_1 && $this->complete) {
+            return "2";
+        } else {
+            return "incomplete";
+        }
+    }
 }
